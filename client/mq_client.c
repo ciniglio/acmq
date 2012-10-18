2bf670c81b3a84523677f883c43e61de870e1ffc
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-10-18 15:54:12 -0400

    Created mq_client struct to internalize connection concerns into push and pop methods.
    Now a consumer will only need to initialize the mq_client struct with
    the hostname and port. the mq_client must be passed to push() and pop().

--- client/mq_client.c
+++ client/mq_client.c
@@ -6,15 +6,33 @@
 
 #define MAX_STRING_LENGTH 1024
 
-int pop(char ** res){
+struct mq_client {
+  char * host;
+  char * port;
+};
+
+struct mq_client * initialize_mq_client(char * host, char *port){
+  struct mq_client *mq = malloc(sizeof(struct mq_client));
+  mq->host = malloc(sizeof(char) * (strlen(host) + 1));
+  mq->port = malloc(sizeof(char) * (strlen(port) + 1));
+  strlcpy(mq->host, host, strlen(host) + 1);
+  strlcpy(mq->port, port, strlen(port) + 1);
+
+  return mq;
+}
+
+int pop(struct mq_client * mq, char ** res){
+  init_connection(mq->host, mq->port);
   if(send_data_through_connection("POP") != 0){
     perror("sending");
+    destroy_connection();
     return 1;
   }
   char * long_res;
 
   if (recv_data_from_connection(&long_res) < 0){
     perror("Recieving");
+    destroy_connection();
     return 1;
   }
 
@@ -23,15 +41,18 @@ int pop(char ** res){
   strlcpy(*res, long_res, len + 1);
   free(long_res);
 
+  destroy_connection();
   return 0;
 }
 
 // buf must be a NULL terminated string
-int push(char * buf){
+int push(struct mq_client * mq, char * buf){
+  init_connection(mq->host, mq->port);
   int len = strnlen(buf, MAX_STRING_LENGTH);
   char * str = malloc(sizeof(char) * (len + 5 + 1));
   if(str == NULL){
     perror("alloc");
+    destroy_connection();
     return 1;
   }
   strlcpy(str, "PUSH ", 6);
@@ -39,6 +60,7 @@ int push(char * buf){
 
   if(send_data_through_connection(str) != 0){
     perror("sending");
+    destroy_connection();
     return 1;
   }
 
@@ -48,25 +70,21 @@ int push(char * buf){
   if(recv_data_from_connection(&res) != 4 ||
      strncmp(res, "ACK", 3) != 0){
     perror("No ACK");
+    destroy_connection();
     return 1;
   }
+  destroy_connection();
   return 0;
 }
 
 int main(int argc, char *argv[]){
-  if(init_connection(argv[1], "3443") != 0){
-    exit(1);
-  }
-  push("this on");
-  destroy_connection();
+  struct mq_client * mq = initialize_mq_client("localhost", "3443");
 
+  push(mq, "this on");
 
-  if(init_connection(argv[1], "3443") != 0){
-    exit(1);
-  }
   char * res;
-  pop(&res);
+  pop(mq, &res);
   printf("result: %s\n", res);
-  destroy_connection();
+  free(res);
   return 0;
 }
