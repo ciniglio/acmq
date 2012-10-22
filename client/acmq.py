bc193f0d13b5bba448f0c6e5b1f81c109e9fff4a
Corey Lynch <coreylynch9@gmail.com>
2012-10-22 18:27:52 -0400

    added self to classes

--- client/acmq.py
+++ client/acmq.py
@@ -3,11 +3,11 @@ from socket import socket, AF_INET, SOCK_STREAM
 
 
 class ACMQ():
-	def __init__(host,port):
+	def __init__(self, host,port):
 		self.host = host
 		self.port = port
 
-	def push(body):
+	def push(self, body):
 		string = json.dumps(body)
 		string = "PUSH %s" % string
 		return_value = transact(string)
@@ -15,11 +15,11 @@ class ACMQ():
 			raise Exception("Pushing failed %s" % return_value)
 		return return_value
 
-	def pop():
+	def pop(self):
 		string = transact("POP\n")
 		return json.loads(string)[0]
 
-	def transact(string):
+	def transact(self, string):
 		s = socket(AF_INET, SOCK_STREAM)
 		s.connect((self.host, self.port))
 		s.send(string)
